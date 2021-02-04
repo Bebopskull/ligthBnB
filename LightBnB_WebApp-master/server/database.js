3e490b6011785bb67fcfd44f1c187796bb9bb112
Nelson Eduardo Vasquez <bebopskull@gmail.com>
2021-02-04 15:50:51 -0500

    add reservations query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -84,8 +84,18 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
+  return pool.query(`
+  SELECT *, properties.* 
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  WHERE reservations.guest_id = $1
+  LIMIT $2
+  `, [guest_id, limit])
+  .then(res => {
+    console.log(res.rows)
+    return res.rows
+  })
+};
 exports.getAllReservations = getAllReservations;
 
 /// Properties
